cc9aaaf6f758ef1e592f8456e9f71b2adff68b3e
majidsaqr <majidsakr86@gmail.com>
2023-03-28 04:44:17 +0200

    update

--- E-Commerce/main.py
+++ E-Commerce/main.py
@@ -1,21 +1,20 @@
-from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
-
-from kivymd.app import MDApp
-
 from kivy.core.window import Window
-
-from kivy.uix.floatlayout import FloatLayout
-
+from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import Screen
+from kivymd.app import MDApp
 from kivymd.uix.relativelayout import MDRelativeLayout
-
 
 Window.size = (350, 560)  # ( Width & Height )
 
 
 class My_Layout(FloatLayout):
     screen_mngr = ObjectProperty(None)
+
+
+class Screen1(Screen):
+    pass
 
 
 class ClickableTextFieldRoundPassword(MDRelativeLayout):
@@ -28,7 +27,20 @@ class ClickableTextFieldRoundEmail(MDRelativeLayout):
     hint_text = StringProperty()
 
 
+class Signup_Screen(Screen):
+    pass
+
+
+class MD_Bottom_Navigation_Screen(Screen):
+    pass
+
+
+class Fgt_pass_Screen(Screen):
+    pass
+
+
 class mainApp(MDApp):
+
     def build(self):
         self.theme_cls.theme_style_switch_animation = True
         self.theme_cls.theme_style_switch_animation_duration = 0.9
@@ -43,6 +55,10 @@ class mainApp(MDApp):
         self.theme_cls.theme_style = (
             "Dark" if self.theme_cls.theme_style == "Light" else "Light"
         )
+
+    def logger(self):
+        x = "majid"
+        self.root.ids.sign_up_field.text = x
 
 
 if __name__ == '__main__':

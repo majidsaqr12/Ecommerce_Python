cb7b51d555e7c5604a04d03b80f04ac7f40e9006
majidsaqr <majidsakr86@gmail.com>
2023-04-10 15:03:48 +0200

    update

--- E-Commerce/main.py
+++ E-Commerce/main.py
@@ -98,6 +98,7 @@ class Show_Product_phone5_Screen(Screen):
 
 
 class MainApp(MDApp):
+
     email = ""
     full_Name = ""
 
@@ -113,45 +114,58 @@ class MainApp(MDApp):
         return Builder.load_file('mainApp.kv')
 
     def login(self):
-        # self.email = self.root.ids.login_email.text
-        self.root.ids.md_login.elevation = 4
-        if self.root.ids.login_email.text == self.email and self.root.ids.login_email.text != "" and self.root.ids.login_email.text.__contains__(
-                '@') and self.root.ids.login_email.text.__contains__('.com'):
-            self.root.ids.email_intent.text = "Email :  " + self.email
-            self.root.ids.name_intent.text = "Name : " + self.full_Name
-            self.root.ids.screen_mngr.current = "home1"
-            self.root.ids.login_email.text = ""
-            self.root.ids.text_field.text = ""
-            print(self.root.ids.login_pass.text)
-            self.root.ids.login_pass.text = ""
-            # kivymd.toast.kivytoast.kivytoast.toast("successfully", [], 10)
-            toast('Successfully')
+        if self.root.ids.login_email.text != "":
+            if self.root.ids.login_email.text.__contains__('@'):
+                if self.root.ids.login_email.text.__contains__('.com'):
+                    if self.root.ids.login_email.text == self.email:
+                        self.root.ids.email_intent.text = "Email :  " + self.email
+                        self.root.ids.name_intent.text = "Name : " + self.full_Name
+                        self.root.ids.screen_mngr.current = "home1"
+                        self.root.ids.login_email.text = ""
+                        self.root.ids.text_field.text = ""
+                        print(self.root.ids.login_pass.text)
+                        self.root.ids.login_pass.text = ""
+                        # kivymd.toast.kivytoast.kivytoast.toast("successfully", [], 10)
+                        toast('Successfully')
+                    else:
+                        toast('Not valid email or password')
+                        self.root.ids.login_email.required = True
+                        self.root.ids.login_email.helper_text = "Not valid"
+                else:
+                    toast('Your email should contain .com')
+            else:
+                toast('Your email should contain @')
         else:
-            toast('please enter correct email or password')
-            self.root.ids.login_email.required = True
-            self.root.ids.login_email.helper_text = "Please enter correct email"
+            toast('Please enter your email')
 
     def sign_up(self):
         file = open("data.dot", "ab")
         self.email = self.root.ids.email_up.text
         self.full_Name = self.root.ids.full_name.text
 
-        if self.email != "" and self.full_Name != "" and self.email.__contains__('@') and self.email.__contains__(
-                '.com'):
-            self.root.ids.screen_mngr.current = "home1"
-            self.root.ids.email_intent.text = "Email :  " + self.email
-            self.root.ids.name_intent.text = "Name : " + self.full_Name
-            self.root.ids.email_up.text = ""
-            self.root.ids.full_name.text = ""
-            toast('Registration completed successfully, Thank you')
-            pickle.dump(self.email, file)
-            file.close()
-
+        if self.full_Name != "":
+            if self.full_Name.__contains__(' '):
+                if self.email != "":
+                    if self.email.__contains__('@'):
+                        if self.email.__contains__('.com'):
+                            self.root.ids.screen_mngr.current = "home1"
+                            self.root.ids.email_intent.text = "Email :  " + self.email
+                            self.root.ids.name_intent.text = "Name : " + self.full_Name
+                            self.root.ids.email_up.text = ""
+                            self.root.ids.full_name.text = ""
+                            toast('Registration completed successfully')
+                            pickle.dump(self.email, file)
+                            file.close()
+                        else:
+                            toast('Your email should contain .com')
+                    else:
+                        toast('Your email should contain @')
+                else:
+                    toast('Please enter your email')
+            else:
+                toast('Your name should contain first name and last name')
         else:
-            self.root.ids.email_up.required = True
-            self.root.ids.email_up.helper_text = "example@gmail.com"
-            self.root.ids.full_name.required = True
-            toast('please complete your info')
+            toast('Please enter your name')
 
     def sign_in_from_sign_up(self):
         self.root.ids.md_login.elevation = 4
